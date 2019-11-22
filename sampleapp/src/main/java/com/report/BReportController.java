1883170f9cdfdbedff3efd13dab41b6ec0fffdb0
masum <optinfra.masum@gmail.com>
2019-11-22 11:05:55 +0600

    Bangla report is running

---
+++ sampleapp/src/main/java/com/report/BReportController.java
@@ -0,0 +1,101 @@
+package com.report;
+
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.annotation.Resource;
+import javax.enterprise.context.RequestScoped;
+import javax.faces.context.FacesContext;
+import javax.inject.Named;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
+
+import net.sf.jasperreports.engine.JRResultSetDataSource;
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.JasperReport;
+import net.sf.jasperreports.engine.util.JRLoader;
+import net.sf.jasperreports.engine.JasperCompileManager;
+
+@Named
+@RequestScoped
+public class BReportController {
+
+	@Resource(lookup ="java:/afs_rpt_ds")
+	private DataSource ds;
+
+	ResultSet resultset;
+
+	private String accountNo;
+
+	public void getReport() throws Exception {
+
+		Map reportParameter = new HashMap<>();
+		reportParameter.put("accountNo", accountNo);
+		System.out.println("do report---------JRRestultSetDataSource-----");
+
+		String reportName = "/report/works.jrxml";
+
+		ServletContext servletContext = (ServletContext) FacesContext
+				.getCurrentInstance().getExternalContext().getContext();
+		String actualPath = servletContext.getRealPath(reportName);
+
+		//JasperReport jasperReport = (JasperReport) JRLoader.loadObjectFromFile(actualPath);
+
+		JasperReport sourceFile = JasperCompileManager.compileReport(actualPath);
+
+
+		Statement stmt = ds.getConnection().createStatement(
+				ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+		String reportsql = "select * from survey";
+		resultset = stmt.executeQuery(reportsql);
+
+		resultset.last();
+		if (resultset.getRow() != 0) {
+			resultset.beforeFirst();
+			JasperPrint jasperPrint = JasperFillManager.fillReport(
+					sourceFile, reportParameter, new JRResultSetDataSource(
+							resultset));
+			 doPDF(jasperPrint);
+
+		} else {
+
+		}
+
+	}
+
+	public void doPDF(JasperPrint jasperPrint) throws Exception {
+
+		HttpServletResponse httpServletResponse = (HttpServletResponse) FacesContext
+				.getCurrentInstance().getExternalContext().getResponse();
+
+		httpServletResponse.setContentType("application/pdf");
+		httpServletResponse.addHeader("Content-disposition","attachment;filename=Breport.pdf");
+		ServletOutputStream servletOutputStream = httpServletResponse.getOutputStream();
+
+		JasperExportManager.exportReportToPdfStream(jasperPrint, servletOutputStream);
+		FacesContext.getCurrentInstance().responseComplete();
+	}
+
+	public ResultSet getResultset() {
+		return resultset;
+	}
+
+	public void setResultset(ResultSet resultset) {
+		this.resultset = resultset;
+	}
+
+	public String getAccountNo() {
+		return accountNo;
+	}
+
+	public void setAccountNo(String accountNo) {
+		this.accountNo = accountNo;
+	}
+
+}
